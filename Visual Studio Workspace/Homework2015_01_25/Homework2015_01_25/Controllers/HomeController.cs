09c0bdceb00dde6f5169353ef664b95c8f706ed4
Sora Jeon <alcoholfrog@gmail.com>
2015-01-25 05:29:40 +0900

    2015-01-25 AM 5:29

---
+++ Visual Studio Workspace/Homework2015_01_25/Homework2015_01_25/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework2015_01_25.Models;
+
+namespace Homework2015_01_25.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Recom()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Recom(Board recom)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                string type = "추천";
+                int year = DateTime.Now.Year;
+                int month = DateTime.Now.Month;
+                int day = DateTime.Now.Day;
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+
+                recom.Type = type;
+                recom.set_date_time(year, month, day, hour, minute, second);
+                Board.Num_of_recom++;
+
+                return View("Thanks", recom);
+            }
+            else
+            {
+                //유효성 검사 오류가 존재한다.
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ViewResult Not_recom()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Not_recom(Board not_recom)
+        {
+            if (ModelState.IsValid)
+            {
+                string type = "비추천";
+                int year = DateTime.Now.Year;
+                int month = DateTime.Now.Month;
+                int day = DateTime.Now.Day;
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+
+                not_recom.set_date_time(year, month, day, hour, minute, second);
+                not_recom.Type = type;
+                Board.Num_of_not_recom++;
+
+                return View("Thanks", not_recom);
+            }
+            else
+            {
+                //유효성 검사 오류가 존재한다.
+                return View();
+            }
+        }
+    }
+
+    
+}
---
+++ Visual Studio Workspace/Homework2015_01_25/Homework2015_01_25/Models/Board.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework2015_01_25.Models
+{
+    public class Board
+    {
+        static public int Num_of_recom;
+        static public int Num_of_not_recom;
+        public string Type { get; set; }
+        
+        [Required(ErrorMessage = "이름을 입력하십시오.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "제목을 입력하십시오.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "본문을 입력하십시오.")]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = "이유를 선택하십시오.")]
+        public string Reason { get; set; }
+
+        public String Date_time;
+
+        public bool set_date_time(int year, int month, int day, 
+            int hour, int minute, int second)
+        {
+            bool result = false;
+            if (Date_time == null)
+            {
+                this.Date_time = year + "년 " + month + "월 " + day + "일 " +
+                                hour + "시 " + minute + "분 " + second + "초";
+                result = true;
+            }
+            return result;            
+        }
+
+        public int get_num_of_recom()
+        {
+            return Num_of_recom;
+        }
+
+        public int get_num_of_not_recom()
+        {
+            return Num_of_not_recom;
+        }
+    }
+}
